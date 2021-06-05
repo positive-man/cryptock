c33462eea940a58ea9122d64c30bd09f57027472
wookjae_jo <wookjae.jo@ybrain.com>
2021-06-06 04:08:32 +0900

    Implement simulating the trading by bollinger band

--- cryptock/examples/example_2.py
+++ cryptock/examples/example_2.py
@@ -5,43 +5,101 @@ import configparser
 
 from binance import ThreadedWebsocketManager, Client
 
-from metric.bollinger import Bollinger
 from datetime import datetime
+from dataclasses import dataclass
+from typing import *
+import logging
+
+from metric.bollinger import Bollinger
+from utils import log
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 config_root = config['root']
 api_key = config_root['api_key']
 api_secret = config_root['api_secret']
+websocket = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
+websocket.start()
+client = Client(api_key=api_key, api_secret=api_secret)
+
+
+@dataclass
+class Kline:
+    open_time: datetime
+    open: float
+    high: float
+    low: float
+    close: float
+    vol: float
+    close_time: datetime
+
+    @classmethod
+    def of(cls, *values):
+        return Kline(
+            open_time=datetime.fromtimestamp(values[0] / 1000),
+            open=float(values[1]),
+            high=float(values[2]),
+            low=float(values[3]),
+            close=float(values[4]),
+            vol=float(values[5]),
+            close_time=datetime.fromtimestamp(values[6] / 1000)
+        )
+
+        return result
+
+
+class Bot:
+    def __init__(self, symbol: str):
+        self.symbol = symbol
+        self.klines: List[Kline] = []
+        self.update(force=True)
+        self.hold = False
+        self.buy_price = 0
+
+    def update(self, force=False):
+        if force or self.klines[-1].close_time < datetime.now():
+            logging.debug('Updating klines...')
+            self.klines = [Kline.of(*raw) for raw in
+                           client.get_klines(symbol=self.symbol, interval=Client.KLINE_INTERVAL_1MINUTE)]
+
+    @classmethod
+    def log_order(cls, *values):
+        logging.critical(', '.join([''] + [str(v) for v in values]))
+
+    def on_message(self, msg):
+        self.update()
+        last = float(msg.get('c'))
+        bollinger = Bollinger.of([k.close for k in self.klines][-19:] + [last])
+
+        if self.hold:
+            if bollinger.mid <= last:
+                self.hold = False
+                margin = last - self.buy_price
+                margin_percent = margin / self.buy_price * 100
+                self.log_order(self.symbol, 'SELL', last, self.buy_price, margin, margin_percent)
+                self.buy_price = 0
+        else:
+            if bollinger.lower >= last:
+                self.hold = True
+                self.buy_price = last
+                self.log_order(self.symbol, 'BUY', last)
+
+    def join(self):
+        websocket.start_symbol_ticker_socket(callback=self.on_message, symbol=self.symbol)
 
 
 def main():
-    symbol = 'BTCUSDT'
+    # 분봉 정보는 어떻게 가져와서 보관할까?
+    all_symbols = [ticker.get('symbol') for ticker in client.get_symbol_ticker()]
+    symbols = [symbol for symbol in all_symbols if symbol.endswith('USDT')]
+    for symbol in symbols:
+        Bot(symbol).join()
 
-    client = Client(api_key=api_key, api_secret=api_secret)
-    websocket = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
-    websocket.start()
-
-    buy_price = 0
-
-    def handle_socket_message(msg: dict):
-        klines = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE)
-        print(datetime.fromtimestamp(klines[-1][0] / 1000))
-        closes=[float(kline[4]) for kline in klines]
-        last=float(msg.get('c'))
-        bollinger = Bollinger.of(price_list=closes, last=last)
-        print(bollinger)
-
-        # 하단 하향 돌파 시, 매수
-        if closes[-1] < bollinger.lower < last:
-            pass
-
-        # 중단 상향 돌파 시, 매도
-
-
-    websocket.start_symbol_ticker_socket(callback=handle_socket_message, symbol=symbol)
     websocket.join()
 
 
 if __name__ == "__main__":
+    log.init(level=logging.INFO)
+    logging.getLogger('websockets').setLevel(logging.INFO)
+    logging.getLogger('urllib3').setLevel(logging.INFO)
     main()
--- cryptock/metric/bollinger.py
+++ cryptock/metric/bollinger.py
@@ -12,21 +12,18 @@ class Bollinger:
     upper: float
     lower: float
     mid: float
-    cur: float
+    close: float
 
     @classmethod
     def of(cls, price_list: List[float]):
-        last_20 = [price for price in price_list[-19:]]
+        last_20 = [price for price in price_list[-20:]]
         ma_20 = avg(last_20)
-        upper = ma_20 + (2 * statistics.stdev(last_20))
-        lower = ma_20 - (2 * statistics.stdev(last_20))
-        close_7 = last_20[-7:]
+        close_7 = price_list[-7:]
         ma_7 = avg(close_7)
-        mid = ma_7
 
         return Bollinger(
-            upper=upper,
-            lower=lower,
-            mid=mid,
-            cur=price_list[-1]
+            upper=ma_20 + (2 * statistics.stdev(last_20)),
+            lower=ma_20 - (2 * statistics.stdev(last_20)),
+            mid=ma_7,
+            close=price_list[-1]
         )
